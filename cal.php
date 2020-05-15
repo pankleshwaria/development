f9b16bfc25121227b361d37d9728fe608c014add
Percy B. Ankleshwaria <percya@gmail.com>
2020-05-15 14:27:54 +0530

    Adding cal file

---
+++ cal.php
@@ -0,0 +1,25 @@
+<?php
+
+class CCal {
+
+	public function __construct() {
+	}
+
+	public function add( $firstNumber, $secondNumber ) {
+		
+	}
+
+	public function subtract( $firstNumber, $secondNumber ) {
+		
+	}
+
+	public function divide( $firstNumber, $secondNumber ) {
+		
+	}
+
+	public function multiply( $firstNumber, $secondNumber ) {
+		
+	}
+	
+}
+?>
